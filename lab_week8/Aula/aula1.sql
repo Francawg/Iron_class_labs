a74184d2064f1b806f4bfae3fe803472c1a27d5f
WgF <wgfranca1987@gmail.com>
2020-08-27 13:36:05 -0300

    Concluindo labs

---
+++ lab_week8/Aula/aula1.sql
@@ -0,0 +1,9 @@
+SELECT building_id,building_name,height FROM buildings;
+
+SELECT * FROM buildings;
+
+SELECT building_id AS buil_id FROM buildings;--Dar nome a uma nova coluna, criar um Alias
+
+SELECT*FROM buildings WHERE height > 400;
+
+
